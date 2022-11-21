424db16af07f27a998393fabdd2761eb883b2782
Adlinassp <safasephia@gmail.com>
2022-11-21 23:07:58 +0800

    Create admin.h

---
+++ src/admin.h
@@ -0,0 +1,13 @@
+#include "admin.cpp"
+
+void tambahdata(node **Head);
+
+void tampildata(node *Head);
+
+void updatedata(node **Head);
+
+void justdelete(node **Head);
+
+void dataadmin();
+
+void loginAdmin();
